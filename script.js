9115c76cda3e7d40410d43352c4f1ec4d96ae8b7
CLedsonB <74743220+CLedsonB@users.noreply.github.com>
2023-09-14 19:01:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var images = ['https://images.srkh.in/wp-content/uploads/2023/02/Happy-Propose-Day-Images-333-I-LOVE-YOU.jpg'];
+var images = ['https://pm1.aminoapps.com/6703/6eb2d6feb2e955f94c433879ffb186d54364c60b_00.jpg'];
 
 var currentIndex = 0;
 var totalClicks = 0;
